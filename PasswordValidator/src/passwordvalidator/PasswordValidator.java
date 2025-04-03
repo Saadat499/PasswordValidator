49d2d48e0f94d8efd92fb2b39120f70d0e29b0a0
Saadat499 <saadat.ahmed499@gmail.com>
2025-04-03 21:46:47 +0800

    Updated PasswordValidator

--- PasswordValidator/src/passwordvalidator/PasswordValidator.java
+++ PasswordValidator/src/passwordvalidator/PasswordValidator.java
@@ -3,18 +3,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package passwordvalidator;
+import java.util.*;
 
-/**
- *
- * @author Saadat
- */
 public class PasswordValidator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+      public static boolean validatePassword(String password){
+        if(password.length()<8){
+            System.out.println("Invalid password");
+            return false;
+        }
+        return true;
+        
+      }
+      public static boolean main(String[]args){
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password:");
+        String password = sc.nextLine();
+        
+        boolean isValid = validatePassword(password);
+        
+        if(isValid){
+            System.out.println("Valid Password");
     }
-    
+        else{
+            System.out.println("Invalid password");
+        }
+        
+        
+      
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+        
+        for(int i = 0;i<password.length();i++){
+        char ch = password.charAt(i);
+        if(Character.isUpperCase(ch)){
+            hasUpperCase = true;
+        }
+        else if(Character.isLowerCase(ch)){
+            hasLowerCase = true;
+        }
+        else if(Character.isDigit(ch)){
+            hasDigit = true;
+        }
+        else if(Character.isSpaceChar(ch)){
+            hasSpecialChar = true;
+        }
+        return hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar;
 }
+          return false;
+        
+}
+}
+
+ 
